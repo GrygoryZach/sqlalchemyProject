be43acfabb1f7a0f80fae455ac10f0d88d178f29
GrygoryZach <grishazschepilo@gmail.com>
2024-03-07 12:12:05 +0100

    Журнал работ

--- data/jobs.py
+++ data/jobs.py
@@ -7,11 +7,11 @@ class Job(SqlAlchemyBase):
     __tablename__ = "jobs"
 
     id = sqlalchemy.Column(sqlalchemy.Integer, autoincrement=True, primary_key=True)
-    team_leader = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
+    team_leader_id = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey('users.id'))
     job = sqlalchemy.Column(sqlalchemy.String)
     work_size = sqlalchemy.Column(sqlalchemy.Integer)
     collaborators = sqlalchemy.Column(sqlalchemy.String)
     start_date = sqlalchemy.Column(sqlalchemy.DateTime, default=datetime.datetime.now)
     end_date = sqlalchemy.Column(sqlalchemy.DateTime)
     is_finished = sqlalchemy.Column(sqlalchemy.Boolean, default=True)
-    user = orm.relationship('User')
+    team_leader = orm.relationship('User')
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 from data import db_session
 from data.jobs import Job
+import web
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
@@ -8,14 +9,22 @@ app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
 
 def main():
     db_session.global_init("db/mars_explorer.db")
-    job = Job()
-    job.team_leader, job.job, job.work_size, job.collaborators, job.is_finished = \
-        [1, "deployment of residential modules 1 and 2", 15, "2, 3", False]
+    # job = Job()
+    # job.team_leader_id, job.job, job.work_size, job.collaborators, job.is_finished = \
+    #     [1, "deployment of residential modules 1 and 2", 15, "2, 3", False]
     db_sess = db_session.create_session()
-    db_sess.add(job)
+    # db_sess.add(job)
     db_sess.commit()
     # app.run()
 
 
+@app.route("/")
+def index():
+    db_sess = db_session.create_session()
+    jobs = db_sess.query(Job)
+    return render_template("index.html", jobs=jobs)
+
+
 if __name__ == '__main__':
     main()
+    app.run(port=8080, host='127.0.0.1')
