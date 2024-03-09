9a49f5471532331301112c572ff47d6a6bc82bac
GrygoryZach <grishazschepilo@gmail.com>
2024-03-09 14:18:13 +0100

    Авторизация и регистрация пользователей

--- forms/user.py
+++ forms/user.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import PasswordField, StringField, IntegerField, TextAreaField, SubmitField, EmailField
+from wtforms import PasswordField, StringField, IntegerField, BooleanField, SubmitField, EmailField
 from wtforms.validators import DataRequired
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -16,8 +16,9 @@ class RegisterForm(FlaskForm):
     address = IntegerField('Address (module number)', validators=[DataRequired()])
     submit = SubmitField('Войти')
 
-    def set_password(self, password):
-        self.hashed_password = generate_password_hash(password)
 
-    def check_password(self, password):
-        return check_password_hash(self.hashed_password, password)
+class LoginForm(FlaskForm):
+    email = EmailField('Почта', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
